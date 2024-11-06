85cbaeaafe10b115dc74d1309b545b648133c6af
Hamody <ahmed.hamody5532@gmail.com>
2024-11-06 18:22:50 +0300

    first commit

---
+++ Filtration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskD08
+{
+    public static class Filtration
+    {
+
+        public static List<Employee> FilterPerId(List<Employee> param)
+        {
+            List<Employee> result = new List<Employee>();
+            foreach (Employee emp in param)
+            {
+
+                if (emp.Id == 10)
+                {
+                    result.Add(emp);
+                }
+            }
+            return result;
+
+        }
+        public static List<Employee> FilterPerSalary(List<Employee> param)
+        {
+            List<Employee> result = new List<Employee>();
+            foreach (Employee emp in param)
+            {
+
+                if (emp.Salary > 3000)
+                {
+                    result.Add(emp);
+                }
+            }
+            return result;
+
+        }
+        public static List<Employee> FilterPer(List<Employee> param, MyDelegate del)
+        {
+            List<Employee> result = new List<Employee>();
+            foreach (Employee emp in param)
+            {
+
+                if (del(emp))
+                {
+                    result.Add(emp);
+                }
+            }
+            return result;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace taskD08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Reposotry.Employees;
+
+
+            var FilteredEmployees = Filtration.FilterPerId(employees);
+            FilteredEmployees = Filtration.FilterPerSalary(employees);
+
+            //MyDelegate del1 = new MyDelegate(Utility.PerDept);
+
+            //MyDelegate del2 =  (Employee emp) =>
+            //{
+            //    return emp.Age > 30;
+            //};
+
+            //MyDelegate del3 = e1 => e1.Age > 30;
+
+            //Employee e1 = new() {Id=1,Name="Sara",Age=50,Salary=1234,DeptId=30 };
+
+            //Console.WriteLine(del2(e1));
+
+
+            FilteredEmployees = Filtration.FilterPer(employees, e1 => e1.Age > 25);
+
+            foreach (var item in FilteredEmployees) {
+
+                Console.WriteLine(item);
+
+            }
+
+
+            Func<int> fun1 = () => 5 + 6;
+            Func< int,int,  bool> fun2 = (x,y) => x > y;
+
+
+            Predicate<Employee> pred = e => e.Salary > 2500;
+
+            Action<int, int> act = (x, y) => Console.WriteLine(x + y);
+        }
+    }
+}
